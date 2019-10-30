b1c0d161ee907594d1e3073d3fc0e338685ec1fa
Chloe Gimenes <chloegimenes@gmail.com>
2019-10-30 17:25:49 +0100

    Main

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -7,7 +7,27 @@ class Main extends Component {
     this.state = {};
   }
   render() {
-    return <div className="Main"></div>;
+    return (
+      <div className="cards-flexbox">
+        <div class="cards">
+          <img
+            src="https://www.podcastscience.fm/wp-content/uploads/2011/12/Untitled.png"
+            alt="Avatar"
+          />
+
+          <div class="cards-container">
+            <h4>
+              <b>John Doe</b>
+            </h4>
+            <p>LEVEL 2</p>
+          </div>
+        </div>
+        <div>
+          <button className="cards-container-buttons">X</button>
+          <button className="cards-container-buttons">GO!</button>
+        </div>
+      </div>
+    );
   }
 }
 
